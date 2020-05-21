d5a2cc3831d92cc21b7f6065ae6b32c70adda190
Dave Gittins <david.gittins@imc.com>
2020-05-21 18:18:37 +1000

    Rename plugin and remove unused bits

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/mattermost/mattermost-plugin-starter-template
+module github.com/imc/mattermost-plugin-pingboard
 
 go 1.12
 
--- server/plugin.go
+++ server/plugin.go
@@ -8,21 +8,14 @@ import (
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
-// Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
 	plugin.MattermostPlugin
 
-	// configurationLock synchronizes access to the configuration.
 	configurationLock sync.RWMutex
 
-	// configuration is the active plugin configuration. Consult getConfiguration and
-	// setConfiguration for usage.
 	configuration *configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func (p *Plugin) ServeHTTP(_ *plugin.Context, w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprint(w, "Hello, world!")
 }
-
-// See https://developers.mattermost.com/extend/plugins/server/reference/
